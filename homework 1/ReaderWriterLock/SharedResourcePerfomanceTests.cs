0a836ccea5f281fc3ca6c70df59201ab77f7473d
kashin.aleksandr <aa.kashin@mail.ru>
2025-01-27 10:18:06 +0500

    Реализация блокировок и тесты

--- homework 1/ReaderWriterLock/SharedResourcePerfomanceTests.cs
+++ homework 1/ReaderWriterLock/SharedResourcePerfomanceTests.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Threading;
 using NUnit.Framework;
 using NUnit.Framework.Legacy;
@@ -31,13 +32,41 @@ public class SharedResourcePerformanceTests
         ClassicAssert.Less(rwLockTime, lockTime, "ReaderWriterLock should be faster than Lock");
     }
 
+    // Нужно реализовать тест производительности.
+    // В многопоточном режиме нужно запустить:
+    // - Чтение общего ресурса в количестве ReadersThreads читающих потоков
+    // - Запись значений в количестве WritersThreads записывающих потоков
+    // - В вызовах читателей и писателей обязательно нужно вызывать подсчет факториала для симуляции полезной нагрузки
     private long MeasurePerformance()
     {
-        // Нужно реализовать тест производительности.
-        // В многопоточном режиме нужно запустить:
-        // - Чтение общего ресурса в количестве ReadersThreads читающих потоков
-        // - Запись значений в количестве WritersThreads записывающих потоков
-        // - В вызовах читателей и писателей обязательно нужно вызывать подсчет факториала для симуляции полезной нагрузки
-        throw new NotImplementedException();
+        var writers = Enumerable.Range(0, WritersThreads)
+            .Select(_ => new Thread(() =>
+            {
+                for (int i = 0; i < NumberOfIterations; i++)
+                {
+                    _sharedResource.Write("");
+                    _sharedResource.ComputeFactorial(FactorialNumber);
+                }
+            }))
+            .ToList();
+        var readers = Enumerable.Range(0, ReadersThreads)
+            .Select(_ => new Thread(() =>
+            {
+                for (int i = 0; i < NumberOfIterations; i++)
+                {
+                    _sharedResource.Read();
+                    _sharedResource.ComputeFactorial(FactorialNumber);
+                }
+            }))
+            .ToList();
+
+        var allThreads = writers.Concat(readers).ToList();
+        var stopwatch = new Stopwatch();
+        stopwatch.Start();
+        allThreads.ForEach(thread => thread.Start());
+        allThreads.ForEach(thread => thread.Join());
+        stopwatch.Stop();
+
+        return stopwatch.ElapsedMilliseconds;
     }
 }
--- homework 1/ReaderWriterLock/SharedResourceRwLock.cs
+++ homework 1/ReaderWriterLock/SharedResourceRwLock.cs
@@ -1,19 +1,41 @@
+using System.Threading;
+
 namespace ReaderWriterLock;
 
 public class SharedResourceRwLock : SharedResourceBase
 {
+    private string _sharedResource = string.Empty;
+    private ReaderWriterLockSlim _rwLock = new();
     public override void Write(string data)
     {
-        throw new System.NotImplementedException();
+        _rwLock.EnterWriteLock();
+        _sharedResource = data;
+        _rwLock.ExitWriteLock();
     }
 
     public override string Read()
     {
-        throw new System.NotImplementedException();
+        try
+        {
+            _rwLock.EnterReadLock();
+            return _sharedResource;
+        }
+        finally
+        {
+            _rwLock.ExitReadLock();
+        }
     }
 
     public override long ComputeFactorial(int number)
     {
-        throw new System.NotImplementedException();
+        _rwLock.EnterReadLock();
+        try
+        {
+            return Factorial(number);
+        }
+        finally
+        {
+            _rwLock.ExitReadLock();
+        }
     }
 }
