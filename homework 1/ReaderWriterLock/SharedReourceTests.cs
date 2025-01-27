aa88509f01fde002426bd7197367c617d15818a1
Nikita Shevyrin <nikita.shevyrin@gmail.com>
2025-01-27 14:45:16 +0500

    finished homework 1

--- homework 1/ReaderWriterLock/SharedReourceTests.cs
+++ homework 1/ReaderWriterLock/SharedReourceTests.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Linq;
 using System.Threading;
 using NUnit.Framework;
@@ -13,23 +12,60 @@ public class SharedResourceTests
     private const int ReadersThreads = 1000;
     private SharedResourceBase _sharedResource;
 
+    private void RunThreads()
+    {
+        var countdown = new CountdownEvent(ReadersThreads + WritersThreads);
+        var startEvent = new ManualResetEventSlim();
+        
+        for (var i = 0; i < WritersThreads; i++)
+        {
+            var number = i;
+            var thread = new Thread(() =>
+            {
+                startEvent.Wait();
+                _sharedResource.Write($"Data {number}");
+                countdown.Signal();
+            });
+            thread.Start();
+        }
+
+        for (var i = 0; i < ReadersThreads; i++)
+        {
+            var thread = new Thread(() =>
+            {
+                startEvent.Wait();
+                var result = _sharedResource.Read();
+                Assert.That(result, Is.Ordered.Ascending);
+                result.ForEach(str =>
+                    ClassicAssert.IsTrue(str.Split(' ')[0] == "Data" && int.TryParse(str.Split(' ')[1], out _)));
+                countdown.Signal();
+            });
+            thread.Start();
+        }
+        
+        startEvent.Set();
+        countdown.Wait();
+    }
+
     [Test]
     public void TestConcurrentReadWrite()
     {
-        // Реализовать проверку конкурентной записи и чтения, где в конце должны проверить что данные последнего потока записаны
-        // Проверка должна быть многопоточной.
-        // Потоков чтения должно быть ReadersThreads, потоков записи должно быть WritersThreads
+        _sharedResource = new SharedResourceLock();
         
-        ClassicAssert.AreEqual($"Data {WritersThreads-1}", _sharedResource.Read());
+        RunThreads();
+        
+        ClassicAssert.AreEqual($"Data {WritersThreads-1}", _sharedResource.Read().Last());
+        ClassicAssert.AreEqual(_sharedResource.Count, WritersThreads);
     }
     
     [Test]
     public void TestConcurrentReadWriteRwLock()
     {
-        // Реализовать проверку конкурентной записи и чтения, где в конце должны проверить что данные последнего потока записаны
-        // Проверка должна быть многопоточной
-        // Потоков чтения должно быть ReadersThreads, потоков записи должно быть WritersThreads
+        _sharedResource = new SharedResourceRwLock();
         
-        ClassicAssert.AreEqual($"Data {WritersThreads-1}", _sharedResource.Read());
+        RunThreads();
+        
+        ClassicAssert.AreEqual($"Data {WritersThreads-1}", _sharedResource.Read().Last());
+        ClassicAssert.AreEqual(_sharedResource.Count, WritersThreads);
     }
 }
