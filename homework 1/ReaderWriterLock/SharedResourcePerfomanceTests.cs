4706e73144a80af2bc079d85317dc5c375c3a52a
vafle228 <dimasmyslaev228@gmail.com>
2025-01-28 18:37:07 +0500

    Перфоманс тест

--- homework 1/ReaderWriterLock/SharedResourcePerfomanceTests.cs
+++ homework 1/ReaderWriterLock/SharedResourcePerfomanceTests.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Threading;
 using NUnit.Framework;
 using NUnit.Framework.Legacy;
@@ -10,34 +11,60 @@ namespace ReaderWriterLock;
 [TestFixture]
 public class SharedResourcePerformanceTests
 {
-    private SharedResourceBase _sharedResource;
     private const int WritersThreads = 100;
     private const int ReadersThreads = 1000;
-    private const int NumberOfIterations = 10000;
+    private const int NumberOfIterations = 1000;
     private const int FactorialNumber = 60; // Большое число для вычисления факториала
 
     [Test]
     public void TestLockPerformance()
     {
-        _sharedResource = new SharedResourceLock();
-        long lockTime = MeasurePerformance();
+        var lockTime = MeasurePerformance(new SharedResourceLock());
+        var rwLockTime = MeasurePerformance(new SharedResourceRwLock());
+        
         Console.WriteLine($"Lock time taken: {lockTime} ms");
-
-        _sharedResource = new SharedResourceRwLock();
-        long rwLockTime = MeasurePerformance();
         Console.WriteLine($"ReaderWriterLock time taken: {rwLockTime} ms");
 
         // Проверка, что время выполнения с ReaderWriterLock меньше, чем с Lock
-        ClassicAssert.Less(rwLockTime, lockTime, "ReaderWriterLock should be faster than Lock");
+        Assert.That(rwLockTime, Is.LessThan(lockTime), "ReaderWriterLock should be faster than Lock");
     }
 
-    private long MeasurePerformance()
+    private static long MeasurePerformance(SharedResourceBase sharedResource)
     {
         // Нужно реализовать тест производительности.
         // В многопоточном режиме нужно запустить:
         // - Чтение общего ресурса в количестве ReadersThreads читающих потоков
         // - Запись значений в количестве WritersThreads записывающих потоков
         // - В вызовах читателей и писателей обязательно нужно вызывать подсчет факториала для симуляции полезной нагрузки
-        throw new NotImplementedException();
+        
+        var writers = Enumerable.Range(0, WritersThreads)
+            .Select(i => new Thread(() =>
+            {
+                for (var _ = 0; _ < NumberOfIterations; _++)
+                {
+                    sharedResource.Write($"Data: {i}");
+                    sharedResource.ComputeFactorial(FactorialNumber);
+                }
+            })).ToList();
+
+        var readers = Enumerable.Range(0, ReadersThreads)
+            .Select(_ => new Thread(() =>
+            {
+                for (var __ = 0; __ < NumberOfIterations; __++)
+                {
+                    sharedResource.Read();
+                    sharedResource.ComputeFactorial(FactorialNumber);
+                }
+            })).ToList();
+        
+        var sw = Stopwatch.StartNew();
+        writers.ForEach(w => w.Start());
+        readers.ForEach(r => r.Start());
+        
+        writers.ForEach(w => w.Join());
+        readers.ForEach(r => r.Join());
+        sw.Stop();
+        
+        return sw.ElapsedMilliseconds;
     }
 }
