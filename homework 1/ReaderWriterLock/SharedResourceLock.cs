9a7ac1df96bde6b89c4c05576272a33d8b62bb20
Geratoptus <shura.bocharov.2004@mail.ru>
2025-01-28 17:40:35 +0500

    feat(SharedResourceLock|SharedResourceRwLock): написал функционал локеров

--- homework 1/ReaderWriterLock/SharedResourceLock.cs
+++ homework 1/ReaderWriterLock/SharedResourceLock.cs
@@ -2,18 +2,29 @@ namespace ReaderWriterLock;
 
 public class SharedResourceLock : SharedResourceBase
 {
+    private string _sharedResource;
+    private readonly object _lockObject = new(); 
     public override void Write(string data)
     {
-        throw new System.NotImplementedException();
+        lock (_lockObject)
+        {
+            _sharedResource = data;
+        }
     }
 
     public override string Read()
     {
-        throw new System.NotImplementedException();
+        lock (_lockObject)
+        {
+            return _sharedResource;
+        }
     }
 
     public override long ComputeFactorial(int number)
     {
-        throw new System.NotImplementedException();
+        lock (_lockObject)
+        {
+            return Factorial(number);
+        }
     }
 }
--- homework 1/ReaderWriterLock/SharedResourceRwLock.cs
+++ homework 1/ReaderWriterLock/SharedResourceRwLock.cs
@@ -1,19 +1,34 @@
+using System.Threading;
+
 namespace ReaderWriterLock;
 
 public class SharedResourceRwLock : SharedResourceBase
 {
+    private readonly ReaderWriterLockSlim _rwLock = new();
+    private string _sharedResource;
+    
     public override void Write(string data)
     {
-        throw new System.NotImplementedException();
+        _rwLock.EnterWriteLock();
+        _sharedResource = data;
+        _rwLock.ExitWriteLock();
     }
 
     public override string Read()
     {
-        throw new System.NotImplementedException();
+        _rwLock.EnterReadLock();
+        var result = _sharedResource;
+        _rwLock.ExitReadLock();
+        
+        return result;
     }
 
     public override long ComputeFactorial(int number)
     {
-        throw new System.NotImplementedException();
+        _rwLock.EnterReadLock();
+        var result = Factorial(number);
+        _rwLock.ExitReadLock();
+
+        return result;
     }
 }
