8d34caa9d58a64a36e597eae0917b2ef0278ad37
Maxim Mineev <mms1401@ya.ru>
2025-01-29 19:49:20 +0500

    RWLock resource

--- homework 1/ReaderWriterLock/SharedResourceRwLock.cs
+++ homework 1/ReaderWriterLock/SharedResourceRwLock.cs
@@ -2,18 +2,21 @@ namespace ReaderWriterLock;
 
 public class SharedResourceRwLock : SharedResourceBase
 {
+    private readonly object lockObject = new();
+    private string resource = "";
+    
     public override void Write(string data)
     {
-        throw new System.NotImplementedException();
+        lock (lockObject) resource = data;
     }
 
     public override string Read()
     {
-        throw new System.NotImplementedException();
+        lock (lockObject) return resource;
     }
 
     public override long ComputeFactorial(int number)
     {
-        throw new System.NotImplementedException();
+        lock(lockObject) return Factorial(number);
     }
 }
