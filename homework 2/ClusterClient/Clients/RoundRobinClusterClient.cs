f99a23ce1e031a3c4a810fa27b147af74147206f
Kostornoj-Dmitrij <D1.imosa@yandex.ru>
2025-02-02 09:42:03 +0300

    Добавил реализацию RoundRobinClusterClient

--- homework 2/ClusterClient/Clients/RoundRobinClusterClient.cs
+++ homework 2/ClusterClient/Clients/RoundRobinClusterClient.cs
@@ -1,23 +1,50 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Net;
 using System.Threading.Tasks;
 using log4net;
 
-namespace ClusterClient.Clients
+namespace ClusterClient.Clients;
+
+public class RoundRobinClusterClient : ClusterClientBase
 {
-    public class RoundRobinClusterClient : ClusterClientBase
+    public RoundRobinClusterClient(string[] replicaAddresses) : base(replicaAddresses)
     {
-        public RoundRobinClusterClient(string[] replicaAddresses) : base(replicaAddresses)
-        {
-        }
-
-        public override Task<string> ProcessRequestAsync(string query, TimeSpan timeout)
-        {
-            throw new NotImplementedException();
-        }
-
-        protected override ILog Log => LogManager.GetLogger(typeof(RoundRobinClusterClient));
     }
+
+    public override async Task<string> ProcessRequestAsync(string query, TimeSpan timeout)
+    {
+        var remainingTime = timeout;
+        var startTime = DateTime.Now;
+
+        foreach (var uri in ReplicaAddresses)
+        {
+            var perReplicaTimeout = TimeSpan.FromMilliseconds(
+                remainingTime.TotalMilliseconds / 
+                (ReplicaAddresses.Length - Array.IndexOf(ReplicaAddresses, uri)));
+
+            try
+            {
+                var request = CreateRequest(uri + "?query=" + query);
+                Log.InfoFormat($"Sending request to {request.RequestUri}");
+
+                var requestTask = ProcessRequestAsync(request);
+                var completedTask = await Task.WhenAny(requestTask, Task.Delay(perReplicaTimeout));
+
+                if (completedTask == requestTask)
+                    return await requestTask;
+            }
+            catch (WebException ex)
+            {
+                Log.WarnFormat($"Request to {uri} failed with {ex.Message}");
+            }
+
+            remainingTime = timeout - (DateTime.Now - startTime);
+            if (remainingTime <= TimeSpan.Zero)
+                break;
+        }
+
+        throw new TimeoutException();
+    }
+
+    protected override ILog Log => LogManager.GetLogger(typeof(RoundRobinClusterClient));
 }
