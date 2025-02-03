b5b3ec89aa63c76e83ddb7ccc8b4c8158040534f
Zakhar <zax_2000@mail.ru>
2025-02-03 22:34:00 +0500

    Реализован черновой вариант RoundRobinClusterClient.

--- homework 2/ClusterClient/Clients/RoundRobinClusterClient.cs
+++ homework 2/ClusterClient/Clients/RoundRobinClusterClient.cs
@@ -1,21 +1,53 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Net;
 using System.Threading.Tasks;
 using log4net;
 
 namespace ClusterClient.Clients
 {
-    public class RoundRobinClusterClient : ClusterClientBase
+    public class RoundRobinClusterClient(string[] replicaAddresses)
+        : ClusterClientBase(replicaAddresses)
     {
-        public RoundRobinClusterClient(string[] replicaAddresses) : base(replicaAddresses)
+        public override async Task<string> ProcessRequestAsync(string query, TimeSpan timeout)
         {
-        }
+            var workingReplicasAdressesCount = ReplicaAddresses.Length;
 
-        public override Task<string> ProcessRequestAsync(string query, TimeSpan timeout)
-        {
-            throw new NotImplementedException();
+            var webRequests = new HttpWebRequest[ReplicaAddresses.Length];
+            for (var i = 0; i < webRequests.Length; i++)
+            {
+                webRequests[i] = CreateRequest($"{ReplicaAddresses[i]}?query={query}");
+            }
+
+            for (var i = 0; i < webRequests.Length; i++)
+            {
+                Log.InfoFormat($"Processing {webRequests[i].RequestUri}");
+                var timeoutPerTask = timeout / workingReplicasAdressesCount;
+                var timeoutTask = Task.Delay(timeoutPerTask);
+                var requestTask = ProcessRequestAsync(webRequests[i]);
+
+                var resultTask = await Task.WhenAny(timeoutTask, requestTask);
+
+                if (resultTask == timeoutTask)
+                {
+                    continue;
+                }
+
+                if (resultTask.IsFaulted)
+                {
+                    workingReplicasAdressesCount -= 1;
+                    if (workingReplicasAdressesCount <= 0)
+                    {
+                        throw new WebException();
+                    }
+                    continue;
+                }
+
+                if (resultTask.IsCompletedSuccessfully)
+                {
+                    return ((Task<string>)resultTask).Result;
+                }
+            }
+            throw new TimeoutException();
         }
 
         protected override ILog Log => LogManager.GetLogger(typeof(RoundRobinClusterClient));
