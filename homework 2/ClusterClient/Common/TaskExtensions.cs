1295c1bc3197c7f0f4f23ed632b2560dd75b4e77
kashin.aleksandr <kashin.aleksandr@itmh.ru>
2025-02-03 22:45:09 +0500

    Сделал метод для запуска Task с таймаутом

---
+++ homework 2/ClusterClient/Common/TaskExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ClusterClient.Common;
+
+public static class TaskExtensions
+{
+	public static async Task<T> RunTaskWithTimeoutAsync<T>(this Task<T> task, CancellationToken token)
+	{
+		var delayTask = Task.Delay(Timeout.Infinite, token);
+
+		if (await Task.WhenAny(task, delayTask) == task)
+			return await task;
+
+		throw new TimeoutException("Task timed out");
+	}
+}
