adb41f86aa6cee172076c2bd34ae2b6a03969aea
Maxim <mbrozovskii@gmail.com>
2025-02-04 15:45:53 +0500

    ParallelClusterClient is done

--- homework 2/ClusterClient/Clients/ParallelClusterClient.cs
+++ homework 2/ClusterClient/Clients/ParallelClusterClient.cs
@@ -1,21 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using log4net;
 
 namespace ClusterClient.Clients
 {
-    public class ParallelClusterClient : ClusterClientBase
+    public class ParallelClusterClient(string[] replicaAddresses) : ClusterClientBase(replicaAddresses)
     {
-        public ParallelClusterClient(string[] replicaAddresses) : base(replicaAddresses)
+        public override async Task<string> ProcessRequestAsync(string query, TimeSpan timeout)
         {
-        }
+            var tasks = ReplicaAddresses
+                .Select(async uri =>
+                {
+                    var webRequest = CreateRequest(uri + "?query=" + query);
+                    try
+                    {
+                        return await ProcessRequestAsync(webRequest);
+                    }
+                    catch (WebException ex)
+                    {
+                        Log.WarnFormat("{0}: {1}", webRequest.RequestUri, ex.Message);
+                    }
 
-        public override Task<string> ProcessRequestAsync(string query, TimeSpan timeout)
-        {
-            throw new NotImplementedException();
+                    return null;
+                })
+                .ToList();
+
+            while (tasks.Count != 0)
+            {
+                var processTask = Task.WhenAny(tasks);
+                var delayTask = Task.Delay(timeout);
+                await Task.WhenAny(processTask, delayTask);
+                if (delayTask.IsCompleted)
+                {
+                    throw new TimeoutException();
+                }
+
+                if (processTask.Result.Result is not null)
+                {
+                    return processTask.Result.Result;
+                }
+                tasks.Remove(processTask.Result);
+            }
+            return null;
         }
 
         protected override ILog Log => LogManager.GetLogger(typeof(ParallelClusterClient));
