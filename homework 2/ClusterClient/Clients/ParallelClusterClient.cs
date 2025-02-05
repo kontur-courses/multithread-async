73749cc7fc164eac7649bed2daa43136438cbc2a
Leonid <rtdddawda2534@yandex.ru>
2025-02-05 18:17:46 +0500

    ParallelClusterClient is done

--- homework 2/ClusterClient/Clients/ParallelClusterClient.cs
+++ homework 2/ClusterClient/Clients/ParallelClusterClient.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using log4net;
@@ -13,9 +14,34 @@ namespace ClusterClient.Clients
         {
         }
 
-        public override Task<string> ProcessRequestAsync(string query, TimeSpan timeout)
+        public override async Task<string> ProcessRequestAsync(string query, TimeSpan timeout)
         {
-            throw new NotImplementedException();
+            var tasks = ReplicaAddresses
+                .Select(uri => CreateRequest(uri + "?query=" + query))
+                .Select(ProcessRequestAsync)
+                .ToList();
+
+            var delayTask = Task.Delay(timeout);
+
+            while (tasks.Count != 0)
+            {
+                var currentTask = Task.WhenAny(tasks);
+
+                await Task.WhenAny(currentTask, delayTask);
+
+                if (delayTask.IsCompleted)
+                {
+                    throw new TimeoutException();
+                }
+
+                if (currentTask.Result.Result != null)
+                {
+                    return currentTask.Result.Result;
+                }
+                tasks.Remove(currentTask.Result);
+            }
+
+            return null;
         }
 
         protected override ILog Log => LogManager.GetLogger(typeof(ParallelClusterClient));
