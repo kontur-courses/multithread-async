fda4f6a6d542e32e1585cf6125f992cdd1e3346c
Artyom <flekshum@gmail.com>
2025-02-05 21:13:55 +0500

    implement Clients in ClusterClient namespace

--- homework 2/ClusterClient/Clients/RoundRobinClusterClient.cs
+++ homework 2/ClusterClient/Clients/RoundRobinClusterClient.cs
@@ -1,23 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using log4net;
 
-namespace ClusterClient.Clients
+namespace ClusterClient.Clients;
+
+public class RoundRobinClusterClient : ClusterClientBase
 {
-    public class RoundRobinClusterClient : ClusterClientBase
+    public RoundRobinClusterClient(string[] replicaAddresses) : base(replicaAddresses)
     {
-        public RoundRobinClusterClient(string[] replicaAddresses) : base(replicaAddresses)
-        {
-        }
-
-        public override Task<string> ProcessRequestAsync(string query, TimeSpan timeout)
-        {
-            throw new NotImplementedException();
-        }
-
-        protected override ILog Log => LogManager.GetLogger(typeof(RoundRobinClusterClient));
     }
+
+    public override async Task<string> ProcessRequestAsync(string query, TimeSpan timeout)
+    {
+        var badRequestCount = 0;
+
+        foreach (var request in ReplicaAddresses.Select(x => CreateRequest(x, query)))
+        {
+            var requestTimeout = timeout / (ReplicaAddresses.Length - badRequestCount);
+
+            Log.InfoFormat($"Processing request {request.RequestUri}");
+
+            var requestTask = ProcessRequestAsync(request);
+            var timeoutTask = Task.Delay(requestTimeout);
+            var finishedTask = await Task.WhenAny(requestTask, timeoutTask);
+
+            if (finishedTask == timeoutTask)
+                continue;
+
+            if (requestTask.IsCompletedSuccessfully)
+                return await requestTask;
+
+            badRequestCount++;
+        }
+
+        throw new TimeoutException();
+    }
+
+    protected override ILog Log => LogManager.GetLogger(typeof(RoundRobinClusterClient));
 }
--- homework 2/ClusterClient/Clients/SmartClusterClient.cs
+++ homework 2/ClusterClient/Clients/SmartClusterClient.cs
@@ -1,23 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using log4net;
+using TaskExtensions = ClusterClient.Extensions.TaskExtensions;
 
-namespace ClusterClient.Clients
+namespace ClusterClient.Clients;
+
+public class SmartClusterClient : ClusterClientBase
 {
-    public class SmartClusterClient : ClusterClientBase
+    public SmartClusterClient(string[] replicaAddresses) : base(replicaAddresses)
     {
-        public SmartClusterClient(string[] replicaAddresses) : base(replicaAddresses)
-        {
-        }
-
-        public override Task<string> ProcessRequestAsync(string query, TimeSpan timeout)
-        {
-            throw new NotImplementedException();
-        }
-
-        protected override ILog Log => LogManager.GetLogger(typeof(SmartClusterClient));
     }
+
+    public override async Task<string> ProcessRequestAsync(string query, TimeSpan timeout)
+    {
+        var requestTasks = new List<Task<string>>();
+        var badRequestCount = 0;
+
+        foreach (var request in ReplicaAddresses.Select(x => CreateRequest(x, query)))
+        {
+            var requestTimeout = timeout / (ReplicaAddresses.Length - badRequestCount);
+            var timeoutTask = TaskExtensions.CreateTaskDelayWithResult<string>(requestTimeout);
+
+            Log.InfoFormat($"Processing request {request.RequestUri}");
+
+            var newRequestTask = ProcessRequestAsync(request);
+            requestTasks.Add(newRequestTask);
+            var finishedTask = await Task.WhenAny(requestTasks.Append(timeoutTask));
+
+            if (finishedTask == timeoutTask)
+                continue;
+
+            if (finishedTask.IsCompletedSuccessfully)
+                return await finishedTask;
+
+            requestTasks.Remove(finishedTask);
+            badRequestCount++;
+        }
+
+        throw new TimeoutException();
+    }
+
+    protected override ILog Log => LogManager.GetLogger(typeof(SmartClusterClient));
 }
