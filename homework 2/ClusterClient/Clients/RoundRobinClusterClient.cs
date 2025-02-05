91d70fd92272c826170986c9c249fa0ca8a0f36c
Geratoptus <shura.bocharov.2004@mail.ru>
2025-02-05 17:08:54 +0500

    feat(RoundRobinClusterClient): реализовал функционал

--- homework 2/ClusterClient/Clients/RoundRobinClusterClient.cs
+++ homework 2/ClusterClient/Clients/RoundRobinClusterClient.cs
@@ -1,21 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using log4net;
 
 namespace ClusterClient.Clients
 {
-    public class RoundRobinClusterClient : ClusterClientBase
+    public class RoundRobinClusterClient(string[] replicaAddresses) : ClusterClientBase(replicaAddresses)
     {
-        public RoundRobinClusterClient(string[] replicaAddresses) : base(replicaAddresses)
+        public override async Task<string> ProcessRequestAsync(string query, TimeSpan timeout)
         {
-        }
+            var tasks = ReplicaAddresses
+                .Select(uri => CreateRequest($"{uri}?query={query}"))
+                .Select((req, i) => (SilentProcessRequestAsync(req), i));
+            
+            foreach (var (task, i) in tasks)
+            {
+                var timeoutTask = Task.Delay(timeout / (ReplicaAddresses.Length - i));
+                
+                var sw = Stopwatch.StartNew();
+                await Task.WhenAny(task, timeoutTask);
+                timeout -= TimeSpan.FromMilliseconds(sw.ElapsedMilliseconds);
+                
+                if (task.IsCompleted && task.Result != null) 
+                    return task.Result;
+            }
 
-        public override Task<string> ProcessRequestAsync(string query, TimeSpan timeout)
-        {
-            throw new NotImplementedException();
+            throw new TimeoutException();
         }
 
         protected override ILog Log => LogManager.GetLogger(typeof(RoundRobinClusterClient));
