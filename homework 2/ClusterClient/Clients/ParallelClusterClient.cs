d1d568bdd3b16cf207ec1de8e713608280231837
Geratoptus <shura.bocharov.2004@mail.ru>
2025-02-05 13:32:35 +0500

    feat(ParallelClusterClient): реализовал функционал

--- homework 2/ClusterClient/Clients/ParallelClusterClient.cs
+++ homework 2/ClusterClient/Clients/ParallelClusterClient.cs
@@ -7,15 +7,30 @@ using log4net;
 
 namespace ClusterClient.Clients
 {
-    public class ParallelClusterClient : ClusterClientBase
+    public class ParallelClusterClient(string[] replicaAddresses) : ClusterClientBase(replicaAddresses)
     {
-        public ParallelClusterClient(string[] replicaAddresses) : base(replicaAddresses)
+        public override async Task<string> ProcessRequestAsync(string query, TimeSpan timeout)
         {
-        }
+            var tasks = ReplicaAddresses
+                .Select(uri => CreateRequest($"{uri}?query={query}"))
+                .Select(SilentProcessRequestAsync)
+                .ToList();
+            var timeoutTask = Task.Delay(timeout);
 
-        public override Task<string> ProcessRequestAsync(string query, TimeSpan timeout)
-        {
-            throw new NotImplementedException();
+            while (tasks.Count != 0)
+            {
+                var processTask = Task.WhenAny(tasks);
+                await Task.WhenAny(timeoutTask, processTask);
+                
+                if (timeoutTask.IsCompleted)
+                    throw new TimeoutException();
+                if (processTask.Result.Result != null)
+                    return processTask.Result.Result;
+                
+                tasks.Remove(processTask.Result);
+            }
+
+            return null;
         }
 
         protected override ILog Log => LogManager.GetLogger(typeof(ParallelClusterClient));
