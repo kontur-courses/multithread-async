4d4608e16b2df0ac1f39caefab4d40f978127521
Leonid <rtdddawda2534@yandex.ru>
2025-02-05 21:17:53 +0500

    RoundRobinClusterClient is done

--- homework 2/ClusterClient/Clients/RoundRobinClusterClient.cs
+++ homework 2/ClusterClient/Clients/RoundRobinClusterClient.cs
@@ -13,9 +13,33 @@ namespace ClusterClient.Clients
         {
         }
 
-        public override Task<string> ProcessRequestAsync(string query, TimeSpan timeout)
+        public override async Task<string> ProcessRequestAsync(string query, TimeSpan timeout)
         {
-            throw new NotImplementedException();
+            var tasks = ReplicaAddresses
+                .Select(uri => CreateRequest(uri + "?query=" + query));
+
+            var replicaCount = ReplicaAddresses.Length;
+
+            foreach (var task in tasks)
+            {
+                var localTimeout = timeout / replicaCount;
+                var currentTask = ProcessRequestAsync(task);
+                var delayTask = Task.Delay(localTimeout);
+
+                await Task.WhenAny(currentTask, delayTask);
+
+                if (delayTask.IsCompleted)
+                {
+                    replicaCount--;
+                    continue;
+                }
+
+                if (currentTask.Result != null)
+                {
+                    return currentTask.Result;
+                }
+            }
+            throw new TimeoutException();
         }
 
         protected override ILog Log => LogManager.GetLogger(typeof(RoundRobinClusterClient));
