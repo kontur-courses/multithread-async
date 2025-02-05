95b397813dbb12cf3fe6667b6a9ca5942376262c
Aleksandr Sazonov <alexxsaz96@gmail.com>
2025-02-05 15:44:50 +0500

    Added parallel client implementation

--- homework 2/ClusterClient/Clients/ParallelClusterClient.cs
+++ homework 2/ClusterClient/Clients/ParallelClusterClient.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using log4net;
 
@@ -9,9 +8,35 @@ namespace ClusterClient.Clients
 {
     public class ParallelClusterClient(string[] replicaAddresses) : ClusterClientBase(replicaAddresses)
     {
-        public override Task<string> ProcessRequestAsync(string query, TimeSpan timeout)
+        public override async Task<string> ProcessRequestAsync(string query, TimeSpan timeout)
         {
-            throw new NotImplementedException();
+            var ctx = new CancellationTokenSource(timeout);
+            
+            var tasks = ReplicaAddresses.Select(async uri =>
+            {
+                var webRequest = CreateRequest($"{uri}?query={query}");
+                
+                Log.InfoFormat($"Processing {uri}");
+
+                await using var registration = ctx.Token.Register(webRequest.Abort);
+                return await ProcessRequestAsync(webRequest);
+            }).ToHashSet();
+            
+            while (tasks.Count > 0)
+            {
+                var completedTask = await Task.WhenAny(tasks);
+                tasks.Remove(completedTask);
+
+                if (!completedTask.IsCompletedSuccessfully) continue;
+                await ctx.CancelAsync();
+                return await completedTask;
+            }
+
+            var timeoutException = new TimeoutException();
+            
+            Log.Error("Timeout", timeoutException);
+
+            throw timeoutException;
         }
 
         protected override ILog Log => LogManager.GetLogger(typeof(ParallelClusterClient));
