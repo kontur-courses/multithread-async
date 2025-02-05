5ad41affcec34d366399764608a00f15c86a7cb3
Leonid <rtdddawda2534@yandex.ru>
2025-02-05 22:35:06 +0500

    SmartClusterClient is done

--- homework 2/ClusterClient/Clients/SmartClusterClient.cs
+++ homework 2/ClusterClient/Clients/SmartClusterClient.cs
@@ -13,9 +13,38 @@ namespace ClusterClient.Clients
         {
         }
 
-        public override Task<string> ProcessRequestAsync(string query, TimeSpan timeout)
+        public override async Task<string> ProcessRequestAsync(string query, TimeSpan timeout)
         {
-            throw new NotImplementedException();
+            var tasks = ReplicaAddresses
+                .Select(uri => CreateRequest(uri + "?query=" + query));
+            var previousTasks = new List<Task<string>>();
+            var replicaCount = ReplicaAddresses.Length;
+
+            foreach (var task in tasks)
+            {
+                var localTimeout = timeout / replicaCount;
+                var currentTask = ProcessRequestAsync(task);
+                var delayTask = Task.Delay(localTimeout);
+
+                previousTasks.Add(currentTask);
+
+                var doneTask = Task.WhenAny(previousTasks);
+
+                await Task.WhenAny(doneTask, delayTask);
+
+                if (delayTask.IsCompleted)
+                {
+                    replicaCount--;
+                    previousTasks.Remove(currentTask);
+                    continue;
+                }
+
+                if (doneTask.Result.Result != null)
+                {
+                    return doneTask.Result.Result;
+                }
+            }
+            throw new TimeoutException();
         }
 
         protected override ILog Log => LogManager.GetLogger(typeof(SmartClusterClient));
