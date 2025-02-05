b79db20022c755f388d66fb19970334a265d8b7c
Braidenbikher Victor <viktor.brai@yandex.ru>
2025-02-05 22:31:37 +0500

    Round - robin realization

--- homework 2/ClusterClient/Clients/RoundRobinClusterClient.cs
+++ homework 2/ClusterClient/Clients/RoundRobinClusterClient.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using log4net;
 
@@ -13,9 +16,31 @@ namespace ClusterClient.Clients
         {
         }
 
-        public override Task<string> ProcessRequestAsync(string query, TimeSpan timeout)
+        public override async Task<string> ProcessRequestAsync(string query, TimeSpan timeout)
         {
-            throw new NotImplementedException();
+            var goodCount = ReplicaAddresses.Length;
+
+            foreach (var replicaAddress in ReplicaAddresses)
+            {
+                var webRequest = CreateRequest(replicaAddress + "?query=" + query);
+                Log.InfoFormat($"Processing {webRequest.RequestUri}");
+
+                try
+                {
+                    return await ProcessRequestAsync(webRequest).WaitAsync(timeout / goodCount);
+                }
+                catch (WebException ex)
+                {
+                    goodCount--;
+                    Log.ErrorFormat($"Request [{webRequest.Address}] failed: {ex.Message}");
+                }
+                catch(Exception ex)
+                {
+                    Log.ErrorFormat($"Request [{webRequest.Address}] failed: {ex.Message}");
+                }
+            }
+
+            throw new TimeoutException("All tasks failed or timeout.");
         }
 
         protected override ILog Log => LogManager.GetLogger(typeof(RoundRobinClusterClient));
