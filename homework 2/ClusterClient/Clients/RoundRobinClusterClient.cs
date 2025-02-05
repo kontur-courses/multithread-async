21a302aa7dd7582aecfb3be2d4c77ed5a800387e
Artyom <flekshum@gmail.com>
2025-02-05 23:50:02 +0500

    add request send to replica addresses by priority in RoundRobinClusterClient.cs

--- homework 2/ClusterClient/Clients/RoundRobinClusterClient.cs
+++ homework 2/ClusterClient/Clients/RoundRobinClusterClient.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using log4net;
@@ -7,23 +9,38 @@ namespace ClusterClient.Clients;
 
 public class RoundRobinClusterClient : ClusterClientBase
 {
+    private readonly Dictionary<string, long> _replicaAddressesStatistics;
+    private readonly Stopwatch _requestTimer = new();
+
     public RoundRobinClusterClient(string[] replicaAddresses) : base(replicaAddresses)
     {
+        _replicaAddressesStatistics = new Dictionary<string, long>(replicaAddresses.Length);
+        foreach (var replicaAddress in replicaAddresses)
+            _replicaAddressesStatistics[replicaAddress] = 0;
     }
 
     public override async Task<string> ProcessRequestAsync(string query, TimeSpan timeout)
     {
         var badRequestCount = 0;
+        var replicaAddressesByPriority = ReplicaAddresses
+            .OrderBy(x => _replicaAddressesStatistics[x])
+            .ToArray();
 
-        foreach (var request in ReplicaAddresses.Select(x => CreateRequest(x, query)))
+        foreach (var replicaAddress in replicaAddressesByPriority)
         {
+            var request = CreateRequest(replicaAddress, query);
             var requestTimeout = timeout / (ReplicaAddresses.Length - badRequestCount);
 
             Log.InfoFormat($"Processing request {request.RequestUri}");
 
             var requestTask = ProcessRequestAsync(request);
             var timeoutTask = Task.Delay(requestTimeout);
+
+            _requestTimer.Restart();
             var finishedTask = await Task.WhenAny(requestTask, timeoutTask);
+            _requestTimer.Stop();
+
+            _replicaAddressesStatistics[replicaAddress] = _requestTimer.ElapsedMilliseconds;
 
             if (finishedTask == timeoutTask)
                 continue;
