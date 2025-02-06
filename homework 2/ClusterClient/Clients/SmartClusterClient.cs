bf858a066388a38856e7e5dad7bccbe952d49da6
stupidnessplusplus <58785209+stupidnessplusplus@users.noreply.github.com>
2025-02-06 14:22:43 +0500

    Complete cluster clients

--- homework 2/ClusterClient/Clients/SmartClusterClient.cs
+++ homework 2/ClusterClient/Clients/SmartClusterClient.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using log4net;
 
@@ -13,9 +12,39 @@ namespace ClusterClient.Clients
         {
         }
 
-        public override Task<string> ProcessRequestAsync(string query, TimeSpan timeout)
+        public override async Task<string> ProcessRequestAsync(string query, TimeSpan timeout)
         {
-            throw new NotImplementedException();
+            var stopwatch = Stopwatch.StartNew();
+            var replicasCount = ReplicaAddresses.Length;
+            var tasks = new List<Task>();
+
+            foreach (var uri in ReplicaAddresses)
+            {
+                var request = CreateRequest($"{uri}?query={query}");
+                var taskTimeout = (timeout - stopwatch.Elapsed) / replicasCount;
+                replicasCount--;
+
+                var resultTask = ProcessRequestAsync(request);
+                var delay = Task.Delay(taskTimeout);
+
+                tasks.Add(resultTask);
+                tasks.Add(delay);
+
+                var completedTask = await Task.WhenAny(tasks);
+                tasks.Remove(delay);
+
+                if (completedTask is Task<string> completedResultTask)
+                {
+                    if (completedResultTask.IsCompletedSuccessfully)
+                    {
+                        return completedResultTask.Result;
+                    }
+
+                    tasks.Remove(completedResultTask);
+                }
+            }
+
+            throw new TimeoutException();
         }
 
         protected override ILog Log => LogManager.GetLogger(typeof(SmartClusterClient));
