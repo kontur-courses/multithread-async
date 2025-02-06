4727a1e48e754a91714a4fbd0daa84ea88cdaa50
Maxim Mineev <mms1401@ya.ru>
2025-02-06 14:15:56 +0500

    Smart cluster client

--- homework 2/ClusterClient/Clients/SmartClusterClient.cs
+++ homework 2/ClusterClient/Clients/SmartClusterClient.cs
@@ -1,18 +1,47 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading;
 using System.Threading.Tasks;
 using log4net;
 
 namespace ClusterClient.Clients;
 
-public class SmartClusterClient : ClusterClientBase
+public class SmartClusterClient(string[] replicaAddresses) : ClusterClientBase(replicaAddresses)
 {
-    public SmartClusterClient(string[] replicaAddresses) : base(replicaAddresses)
+    public override async Task<string> ProcessRequestAsync(string query, TimeSpan timeout)
     {
-    }
+        var devidedTimeout = TimeSpan.FromMilliseconds(timeout.TotalMilliseconds / ReplicaAddresses.Length);
+        var badRequestsCount = 0;
+        var tasks = ReplicaAddresses
+            .Select(uri => CreateRequest(uri + "?query=" + query))
+            .Select(request =>
+            {
+                Log.InfoFormat($"Processing {request.RequestUri}");
+                return ProcessRequestAsync(request);
+            });
+        var runTasks = new HashSet<Task<string>>();
 
-    public override Task<string> ProcessRequestAsync(string query, TimeSpan timeout)
-    {
-        throw new NotImplementedException();
+        foreach (var task in tasks)
+        {
+            using var cts = new CancellationTokenSource(devidedTimeout);
+            runTasks.Add(task);
+            var previousTasks = Task.WhenAny(runTasks);
+            await Task.WhenAny(previousTasks, Task.Delay(devidedTimeout, cts.Token));
+            if (!previousTasks.IsCompleted) 
+                continue;
+            runTasks.Remove(previousTasks.Result);
+            if (previousTasks.Result.IsCompletedSuccessfully)
+                return previousTasks.Result.Result;
+            if (!previousTasks.Result.IsFaulted) 
+                continue;
+            badRequestsCount++;
+            devidedTimeout = TimeSpan.FromMilliseconds(timeout.TotalMilliseconds / (ReplicaAddresses.Length - badRequestsCount));
+        }
+        
+        throw new TimeoutException("Task timed out");
+        
     }
 
     protected override ILog Log => LogManager.GetLogger(typeof(SmartClusterClient));
