35cdafc0476e5fc420dcfe8e519ad50f9f45809b
qreaqtor <qreaqtor@gmail.com>
2025-02-06 07:40:56 +0500

    add priority to clients

---
+++ homework 2/ClusterClient/Clients/Priority/ReplicaItemExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ClusterClient.Clients.Priority
+{
+	static class ReplicaItemExtensions
+	{
+		public static void ConcatNeighbors(this ReplicaItem replica)
+		{
+			if (replica.Previous != null)
+				replica.Previous.Next = replica.Next;
+
+			if (replica.Next != null)
+				replica.Next.Previous = replica.Previous;
+		}
+
+		public static bool TryFindNextItem(this ReplicaItem replica, out ReplicaItem target, Func<ReplicaItem, bool> isFind)
+		{
+			var current = replica;
+			while (current != null)
+			{
+				if (isFind(current))
+					break; 
+				current = current.Next;
+			}
+
+			target = current;
+			return target != null;
+		}
+	}
+}
---
+++ homework 2/ClusterClient/Clients/Priority/Request.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClusterClient.Clients.Priority
+{
+	public readonly struct Request(TimeSpan time, bool isSuccess)
+	{
+		public bool IsSuccess => isSuccess;
+		public TimeSpan Time => time;
+	}
+}
