bdcc7fcf0d9c9bdd2ced261ae1e45fc85949990b
Савелий <vildanov.s@bk.ru>
2025-02-07 19:06:46 +0500

    Add additional task

--- homework 2/ClusterClient/Clients/RoundRobinClusterClient.cs
+++ homework 2/ClusterClient/Clients/RoundRobinClusterClient.cs
@@ -2,36 +2,61 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Net;
-using System.Text;
 using System.Threading.Tasks;
 using log4net;
 
-namespace ClusterClient.Clients
+namespace ClusterClient.Clients;
+
+public class RoundRobinClusterClient(string[] replicaAddresses) : ClusterClientBase(replicaAddresses)
 {
-    public class RoundRobinClusterClient(string[] replicaAddresses) : ClusterClientBase(replicaAddresses)
+    private readonly Dictionary<string, List<TimeSpan>> _replicaResponseTimes = new();
+
+    public override async Task<string> ProcessRequestAsync(string query, TimeSpan timeout)
     {
-        public override async Task<string> ProcessRequestAsync(string query, TimeSpan timeout)
+        var countOfReplicas = ReplicaAddresses.Length;
+        var sortedReplicas = ReplicaAddresses
+            .Select(uri => new {Uri = uri, AvgResponseTime = GetAverageResponseTime(uri)})
+            .OrderBy(x => x.AvgResponseTime)
+            .ToList();
+
+        foreach (var replica in sortedReplicas)
         {
-            var countOfReplicas = ReplicaAddresses.Length;
-            foreach (var uri in ReplicaAddresses)
-            {
-                var perReplicaTimeout = timeout / countOfReplicas--;
-                var webRequest = CreateRequest(uri + "?query=" + query);
-                var task = ProcessRequestAsync(webRequest);
-                var sw = Stopwatch.StartNew();
-                var completedTask = await Task.WhenAny(task, Task.Delay(perReplicaTimeout));
-                timeout -= TimeSpan.FromMilliseconds(sw.ElapsedMilliseconds);
+            var uri = replica.Uri;
+            var perReplicaTimeout = timeout / countOfReplicas--;
+            var webRequest = CreateRequest(uri + "?query=" + query);
+            var task = ProcessRequestAsync(webRequest);
+            var sw = Stopwatch.StartNew();
+            var completedTask = await Task.WhenAny(task, Task.Delay(perReplicaTimeout));
+            timeout -= TimeSpan.FromMilliseconds(sw.ElapsedMilliseconds);
 
-                if (completedTask == task && task.IsCompletedSuccessfully && !string.IsNullOrEmpty(task.Result))
-                {
-                    return task.Result;
-                }
-            }
-
-            throw new TimeoutException();
+            if (completedTask != task || !task.IsCompletedSuccessfully || string.IsNullOrEmpty(task.Result)) continue;
+            UpdateResponseTime(uri, sw.Elapsed);
+            return task.Result;
         }
 
-        protected override ILog Log => LogManager.GetLogger(typeof(RoundRobinClusterClient));
+        throw new TimeoutException();
     }
+
+    private TimeSpan GetAverageResponseTime(string replicaUri)
+    {
+        if (_replicaResponseTimes.TryGetValue(replicaUri, out List<TimeSpan> value) && value.Count > 0)
+        {
+            return TimeSpan.FromMilliseconds(value.Average(t => t.TotalMilliseconds));
+        }
+
+        return TimeSpan.MinValue;
+    }
+
+    private void UpdateResponseTime(string replicaUri, TimeSpan responseTime)
+    {
+        if (!_replicaResponseTimes.TryGetValue(replicaUri, out var value))
+        {
+            value = [];
+            _replicaResponseTimes[replicaUri] = value;
+        }
+
+        value.Add(responseTime);
+    }
+
+    protected override ILog Log => LogManager.GetLogger(typeof(RoundRobinClusterClient));
 }
--- homework 2/ClusterClient/Clients/SmartClusterClient.cs
+++ homework 2/ClusterClient/Clients/SmartClusterClient.cs
@@ -6,37 +6,67 @@ using System.Text;
 using System.Threading.Tasks;
 using log4net;
 
-namespace ClusterClient.Clients
+namespace ClusterClient.Clients;
+
+public class SmartClusterClient(string[] replicaAddresses) : ClusterClientBase(replicaAddresses)
 {
-    public class SmartClusterClient(string[] replicaAddresses) : ClusterClientBase(replicaAddresses)
+    private readonly Dictionary<string, List<TimeSpan>> _replicaResponseTimes = new();
+
+    public override async Task<string> ProcessRequestAsync(string query, TimeSpan timeout)
     {
-        public override async Task<string> ProcessRequestAsync(string query, TimeSpan timeout)
+        var countOfReplicas = ReplicaAddresses.Length;
+        var workingTasks = new List<Task<string>>();
+
+        var sortedReplicas = ReplicaAddresses
+            .Select(uri => new {Uri = uri, AvgResponseTime = GetAverageResponseTime(uri)})
+            .OrderBy(x => x.AvgResponseTime)
+            .ToList();
+
+        foreach (var replica in sortedReplicas)
         {
-            var countOfReplicas = ReplicaAddresses.Length;
-            var workingTasks = new List<Task<string>>();
-            foreach (var uri in ReplicaAddresses)
+            var uri = replica.Uri;
+            var perReplicaTimeout = timeout / countOfReplicas--;
+            var webRequest = CreateRequest(uri + "?query=" + query);
+            var task = ProcessRequestAsync(webRequest);
+            workingTasks.Add(task);
+
+            var sw = Stopwatch.StartNew();
+            var completedTask = await Task.WhenAny(workingTasks.Append(Task.Delay(perReplicaTimeout)));
+            timeout -= TimeSpan.FromMilliseconds(sw.ElapsedMilliseconds);
+
+            if (completedTask is not Task<string> resultTask) continue;
+            if (resultTask.IsCompletedSuccessfully && resultTask.Result is not null)
             {
-                var perReplicaTimeout = timeout / countOfReplicas--;
-                var webRequest = CreateRequest(uri + "?query=" + query);
-                var task = ProcessRequestAsync(webRequest);
-                workingTasks.Add(task);
-
-                var sw = Stopwatch.StartNew();
-                var completedTask = await Task.WhenAny(workingTasks.Append(Task.Delay(perReplicaTimeout)));
-                timeout -= TimeSpan.FromMilliseconds(sw.ElapsedMilliseconds);
-
-                if (completedTask is not Task<string> resultTask) continue;
-                if (resultTask.IsCompletedSuccessfully && resultTask.Result is not null)
-                {
-                    return resultTask.Result;
-                }
-
-                workingTasks.Remove(resultTask);
+                UpdateResponseTime(uri, sw.Elapsed);
+                return resultTask.Result;
             }
 
-            throw new TimeoutException("Request to the replicas timed out.");
+            workingTasks.Remove(resultTask);
         }
 
-        protected override ILog Log => LogManager.GetLogger(typeof(SmartClusterClient));
+        throw new TimeoutException("Request to the replicas timed out.");
     }
+
+    private TimeSpan GetAverageResponseTime(string replicaUri)
+    {
+        if (_replicaResponseTimes.TryGetValue(replicaUri, out List<TimeSpan> value) && value.Count > 0)
+        {
+            return TimeSpan.FromMilliseconds(value.Average(t => t.TotalMilliseconds));
+        }
+
+        return TimeSpan.MinValue;
+    }
+
+    private void UpdateResponseTime(string replicaUri, TimeSpan responseTime)
+    {
+        if (!_replicaResponseTimes.TryGetValue(replicaUri, out List<TimeSpan> value))
+        {
+            value = [];
+            _replicaResponseTimes[replicaUri] = value;
+        }
+
+        value.Add(responseTime);
+    }
+
+    protected override ILog Log => LogManager.GetLogger(typeof(SmartClusterClient));
 }
