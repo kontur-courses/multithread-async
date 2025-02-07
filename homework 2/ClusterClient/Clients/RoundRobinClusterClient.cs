6749caf694e1a0b6f0cd2f68de22ebbd928a750c
Dmitriy Bessarab <bessarabds@gmail.com>
2025-02-07 22:37:00 +0500

    add RoundRobinClusterClient

--- homework 2/ClusterClient/Clients/RoundRobinClusterClient.cs
+++ homework 2/ClusterClient/Clients/RoundRobinClusterClient.cs
@@ -1,23 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using log4net;
 
-namespace ClusterClient.Clients
+namespace ClusterClient.Clients;
+
+public class RoundRobinClusterClient(string[] replicaAddresses) : ClusterClientBase(replicaAddresses)
 {
-    public class RoundRobinClusterClient : ClusterClientBase
+    public override async Task<string> ProcessRequestAsync(string query, TimeSpan timeout)
     {
-        public RoundRobinClusterClient(string[] replicaAddresses) : base(replicaAddresses)
-        {
-        }
+        var timeLimit = timeout;
+        var timer = new Stopwatch();
+        var totalCount = ReplicaAddresses.Length;
+        var requestTimeout = timeLimit / ReplicaAddresses.Length;
 
-        public override Task<string> ProcessRequestAsync(string query, TimeSpan timeout)
+        foreach (var replicaAddress in ReplicaAddresses)
         {
-            throw new NotImplementedException();
+            var requestTask = ProcessRequestAsync(replicaAddress, query);
+            var delayTask = Task.Delay(requestTimeout);
+            timer.Restart();
+            var resultTask = await Task.WhenAny(requestTask, delayTask);
+            timer.Stop();
+            if (resultTask != delayTask && !resultTask.IsFaulted)
+                return await requestTask;
+            totalCount -= 1;
+            timeLimit -= timer.Elapsed;
+            if (totalCount > 0)
+                requestTimeout = timeLimit / totalCount;
         }
-
-        protected override ILog Log => LogManager.GetLogger(typeof(RoundRobinClusterClient));
+        throw new TimeoutException();
     }
+
+    private async Task<string> ProcessRequestAsync(string replicaAddress, string query)
+    {
+        var request = CreateRequest(replicaAddress + "?query=" + query);
+        Log.InfoFormat($"Processing {request.RequestUri}");
+        return await ProcessRequestAsync(request);
+    }
+
+    protected override ILog Log => LogManager.GetLogger(typeof(RoundRobinClusterClient));
 }
