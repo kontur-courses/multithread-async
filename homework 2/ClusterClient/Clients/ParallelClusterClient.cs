f5f45b3a530356ad717576e981697a32ee87f227
crycrash <monstermusic.club@yandex.ru>
2025-02-18 14:33:41 +0500

    Parallel client

--- homework 2/ClusterClient/Clients/ParallelClusterClient.cs
+++ homework 2/ClusterClient/Clients/ParallelClusterClient.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Fclp.Internals.Extensions;
 using log4net;
 
 namespace ClusterClient.Clients
@@ -13,9 +14,36 @@ namespace ClusterClient.Clients
         {
         }
 
-        public override Task<string> ProcessRequestAsync(string query, TimeSpan timeout)
+        public override async Task<string> ProcessRequestAsync(string query, TimeSpan timeout)
         {
-            throw new NotImplementedException();
+            var tasks = new List<Task>();
+            foreach (var uri in ReplicaAddresses)
+            {
+                var webRequest = CreateRequest(uri + "?query=" + query);
+                Log.InfoFormat($"Processing {webRequest.RequestUri}");
+                tasks.Add(ProcessRequestAsync(webRequest));
+            }
+            
+            var timeoutTask = Task.Delay(timeout);
+
+            while (!tasks.IsNullOrEmpty())
+            {
+                var completedTask = await Task.WhenAny(tasks.Append(timeoutTask));
+
+                if (completedTask == timeoutTask)
+                {
+                    throw new TimeoutException();
+                }
+
+                if (completedTask.IsCompletedSuccessfully)
+                {
+                    return await (Task<string>)completedTask;
+                }
+
+                tasks.Remove(completedTask);
+            }
+
+            throw new TimeoutException();
         }
 
         protected override ILog Log => LogManager.GetLogger(typeof(ParallelClusterClient));
