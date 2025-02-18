b0d516826ace9d4170901d4a86164c81c0d1de84
crycrash <monstermusic.club@yandex.ru>
2025-02-18 17:23:31 +0500

    SmartCluster client

--- homework 2/ClusterClient/Clients/SmartClusterClient.cs
+++ homework 2/ClusterClient/Clients/SmartClusterClient.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,9 +14,28 @@ namespace ClusterClient.Clients
         {
         }
 
-        public override Task<string> ProcessRequestAsync(string query, TimeSpan timeout)
+        public override async Task<string> ProcessRequestAsync(string query, TimeSpan timeout)
         {
-            throw new NotImplementedException();
+            var count = ReplicaAddresses.Length;
+            var tasks = new List<Task<string>>();
+            foreach (var uri in ReplicaAddresses)
+            {
+                var specialTimeout = timeout / count--;
+                var webRequest = CreateRequest(uri + "?query=" + query);
+                Log.InfoFormat($"Processing {webRequest.RequestUri}");
+                var resultTask = ProcessRequestAsync(webRequest);
+                tasks.Add(resultTask);
+                var stopWatch = Stopwatch.StartNew();
+                var completedTask = await Task.WhenAny(tasks.Append(Task.Delay(specialTimeout)));
+                timeout -= TimeSpan.FromMilliseconds(stopWatch.ElapsedMilliseconds);
+                if (completedTask is not Task<string>)
+                    continue;
+                if (completedTask.IsCompletedSuccessfully)
+                    return await (Task<string>)completedTask;
+
+                tasks.Remove(resultTask);
+            }
+            throw new TimeoutException();
         }
 
         protected override ILog Log => LogManager.GetLogger(typeof(SmartClusterClient));
